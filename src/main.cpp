6df260a6e0eabb0338d50eafc96606b51a421a28
Ivo <ivanaraolaza@gmail.com>
2022-08-27 10:50:46 -0300

    Control de brillo rompe el código Al intentar implementar el control de brillo para el LED RGB, el led ya no funciona. El sensor sigue trabajando normalmente. Los datos del control del LED se reciben correctamente y se muestran en el Serial Port, pero al medir los pines, no todos están entregando energía.

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@ void setup(){
   pinMode(GREEN_PIN, OUTPUT);
   pinMode(BLUE_PIN, OUTPUT);
 
+
+
   // ------------------------------------------
   //            Configuración PWM
   // ------------------------------------------
@@ -19,9 +21,12 @@ void setup(){
   ledcSetup(ledChannel_R, pwm_frec, resolution);  // Configura el canal PWM
   ledcSetup(ledChannel_G, pwm_frec, resolution);
   ledcSetup(ledChannel_B, pwm_frec, resolution);
+  ledcSetup(ledChannel_brigth, pwm_frec, resolution);
   ledcAttachPin(RED_PIN, ledChannel_R);           // Asigna el pin al canal PWM.
   ledcAttachPin(GREEN_PIN, ledChannel_G);
   ledcAttachPin(BLUE_PIN, ledChannel_B);
+  ledcAttachPin(BRIGHT_PIN, ledChannel_brigth);
+
   delay(10);
 
   Serial.begin(115200);
@@ -29,7 +34,7 @@ void setup(){
   wifiInit();
   mqttClient.setServer(server, port);
   mqttClient.setCallback(callback);
-  Serial.println("Sensor Chip ............. AM2320");
+  if(debug) Serial.println("Sensor Chip ............. AM2320");
 }
 
 void loop(){
@@ -37,17 +42,13 @@ void loop(){
     reconnect();
   }
 
-  if((millis() - sensorPreviousTime) > 1500) { // Sensa cada 3 segundos
+  if((millis() - sensorPreviousTime) > 1500) { // Sensa y publica cada 1.5 segundos
     sensorPreviousTime = millis(); 
     am2320_sensor();
-  }
-  if((millis() - sendDataPreviousTime) > 1500) { // Publica cada 3 segundos
-    sendDataPreviousTime = millis();
     sendData();
   }
 
   mqttClient.loop();
-
   controlRGB();
 }
 
@@ -75,56 +76,65 @@ void callback(char* topic, byte* payload, unsigned int length) {
   char payload_stringR[length + 1];
   char payload_stringG[length + 1];
   char payload_stringB[length + 1];
-  // ! char payload_string[length + 1];
+  char payload_string_bright[length + 1];
 
  for (size_t i = 0; i < length; i++) { // Crea el payload, concatenando los datos y luego lo convierte de byte a string
     incoming.concat((char)payload[i]);
   }
   Serial.printf("Mensaje recibido ->  %s\n", topic);
-  Serial.println("Mensaje-> " + incoming ); //imprime el mensaje que recibe como string
+  if(debug)Serial.println("Mensaje-> " + incoming ); // Imprime el mensaje que recibe como string
 
 
-  if (strcmp(topic, topic_subscribe_SW) == 0){ // Comprueba si se está suscripto al topic en que se publica
-    led_switch_payload = incoming.toInt(); // Convierte el payload de string a int
-    Serial.printf("Mensaje Brightness -> %i\n", led_switch_payload );
+  // if (strcmp(topic, topic_subscribe_SW) == 0){ // Comprueba si se está suscripto al topic en que se publica
+  //   switch_payload = incoming.toInt(); // Convierte el payload de string a int
+  //   Serial.printf("Mensaje Switch -> %i\n", switch_payload );
+  // }
+
+  if (strcmp(topic, topic_subscribe_bright) == 0){  // Comprueba si se está suscripto al topic en que se publica
+    memcpy(payload_string_bright, payload, length);
+    payload_string_bright[length] = '\0';
+    led_brightness_payload = atoi(payload_string_bright); // Convierte el payload de string a int
+    Serial.printf("Mensaje Brillo -> %i\n", led_brightness_payload);
   }
+
+  else if (strcmp(topic, topic_subscribe_RED) == 0){
+    memcpy(payload_stringR, payload, length);
+    payload_stringR[length] = '\0';
+    red_payload = atoi(payload_stringR);
+    Serial.printf("Mensaje Rojo -> %i\n", red_payload);
+  }
+
   else if (strcmp(topic, topic_subscribe_GREEN) == 0){
     memcpy(payload_stringG, payload, length);
     payload_stringG[length] = '\0';
     green_payload = atoi(payload_stringG);
-    Serial.printf("Mensaje Green-> %i\n", green_payload);
-    // ! Serial.println(green_payload);
+    Serial.printf("Mensaje Verde-> %i\n", green_payload);
   }
   else if (strcmp(topic, topic_subscribe_BLUE) == 0){
     memcpy(payload_stringB, payload, length);
     payload_stringB[length] = '\0';
     blue_payload = atoi(payload_stringB);
-    Serial.printf("Mensaje Blue -> %i\n", blue_payload);
-    // ! Serial.println(blue_payload);
+    Serial.printf("Mensaje Azul -> %i\n", blue_payload);
   }
-  else if (strcmp(topic, topic_subscribe_SW) == 0){
-    led_brightness_payload = incoming.toInt(); //convierte el valor del sw en int
-    Serial.printf("Mensaje Brightness -> %i\n", led_brightness_payload);
-    //! Serial.println(led_brightness_payload);
-  }
-  else if (strcmp(topic, topic_subscribe_Color) == 0){
-    incoming.remove(0, 4);
-    incoming.remove(incoming.length() - 1);
-    char incomingArray[50];
-    incoming.toCharArray(incomingArray, 25);
-    char* rgb[9];
-    char* ptr = NULL;
-    byte index = 0;
-    ptr = strtok(incomingArray, ", ");
-    while (ptr != NULL) {
-      rgb[index] = ptr;
-      index++;
-      ptr = strtok(NULL, ", ");
-    }
-    red_payload = atoi(rgb[0]);
-    green_payload = atoi(rgb[1]);
-    blue_payload = atoi(rgb[2]);
-  }
+  
+  // else if (strcmp(topic, topic_subscribe_Color) == 0){
+  //   incoming.remove(0, 4);
+  //   incoming.remove(incoming.length() - 1);
+  //   char incomingArray[50];
+  //   incoming.toCharArray(incomingArray, 25);
+  //   char* rgb[9];
+  //   char* ptr = NULL;
+  //   byte index = 0;
+  //   ptr = strtok(incomingArray, ", ");
+  //   while (ptr != NULL) {
+  //     rgb[index] = ptr;
+  //     index++;
+  //     ptr = strtok(NULL, ", ");
+  //   }
+  //   red_payload = atoi(rgb[0]);
+  //   green_payload = atoi(rgb[1]);
+  //   blue_payload = atoi(rgb[2]);
+  // }
 }
 
 void reconnect() {
@@ -168,15 +178,16 @@ void am2320_sensor() {
 }
 
 void controlRGB(){
-  if(led_brightness_payload >= 1){        // Si el switch esta activado, envia los datos del pwm al led
+  // if(switch_payload >= 1){   // Si el switch está activado, enciende el LED 
+    ledcWrite(ledChannel_brigth, led_brightness_payload);
     ledcWrite(ledChannel_R, red_payload);
     ledcWrite(ledChannel_G, green_payload);
     ledcWrite(ledChannel_B, blue_payload);
-  }
-  else if(led_brightness_payload == 0){    // Apaga led
-    ledcWrite(ledChannel_R, 0);
-    ledcWrite(ledChannel_G, 0);
-    ledcWrite(ledChannel_B, 0);    
-  }
+  // }
 
+  // else if(switch_payload == 0){    // Apaga led
+  //   ledcWrite(ledChannel_R, 0);
+  //   ledcWrite(ledChannel_G, 0);
+  //   ledcWrite(ledChannel_B, 0);    
+  // }
 }
--- src/main.h
+++ src/main.h
@@ -6,6 +6,7 @@
 // *                PINES   
 // * --------------------------------------
 
+#define BRIGHT_PIN 27
 #define RED_PIN   13
 #define GREEN_PIN 12
 #define BLUE_PIN  14
@@ -17,7 +18,8 @@
 byte red_payload = 0;
 byte green_payload = 0;
 byte blue_payload = 0;
-byte led_switch_payload = 0;
+byte led_brightness_payload = 0;
+// byte switch_payload = 0;
 float temperature = 0;
 float humidity = 0;
 char sensed_temperature[10];    // Buffers para enviar la data. Ver https://arduino.stackexchange.com/questions/26832/how-do-i-convert-a-float-into-char
@@ -32,6 +34,7 @@ const int pwm_frec = 5000;
 const int ledChannel_R = 0;
 const int ledChannel_G = 1;
 const int ledChannel_B = 2;
+const int ledChannel_brigth = 3;
 const int resolution = 8;
 
 // * -------------------------------
@@ -51,14 +54,14 @@ unsigned long sensorPreviousTime = 0;
 // -------------------------------------
 // TODO: Descomentar las credenciales que correspondan
 
-// const char* ssid     = "Fibertel WiFi666 2.4GHz";
-// const char* password = "0044304973";
+const char* ssid     = "Fibertel WiFi666 2.4GHz";
+const char* password = "0044304973";
 // const char* ssid     = "Ivoo";
 // const char* password = "f42xiS3Hj";
 // const char* ssid     = "Wifi 1234";
 // const char* password = "carapezza3574";
-const char* ssid     = "S11";
-const char* password = "ger12345";
+// const char* ssid     = "S11";
+// const char* password = "ger12345";
 
 // --------------------------------------
 //                  MQTT
@@ -67,8 +70,8 @@ const char* server = "broker.hivemq.com";
 const int port = 1883;
 const char* Client_ID = "kjaf1234hgb12ghj54l234hgj15gl5uyg";
 const char* topic_subscribe = "/et28/redes/ig/led/#";
-const char* topic_subscribe_Color = "/et28/redes/ig/led/cp";
-const char* topic_subscribe_SW = "/et28/redes/ig/led/sw";
+const char* topic_subscribe_bright = "/et28/redes/ig/led/bright";
+// const char* topic_subscribe_SW = "/et28/redes/ig/led/sw";
 const char* topic_subscribe_RED = "/et28/redes/ig/led/R";
 const char* topic_subscribe_GREEN = "/et28/redes/ig/led/G";
 const char* topic_subscribe_BLUE = "/et28/redes/ig/led/B";
